98c80f21bb667901a2cde79b687cad5b68cd9340
Derek Ross <derekross@gmail.com>
2025-11-28 07:48:00 -0500

    fix: blossom nostr.download

--- src/hooks/useBlossomServers.ts
+++ src/hooks/useBlossomServers.ts
@@ -86,6 +86,7 @@ export function useBlossomServers() {
   // Get combined server list (user + defaults)
   const getAllServers = () => {
     const defaultServers = [
+      'https://nostr.download',
       'https://blossom.band'
     ];
 
@@ -108,6 +109,7 @@ export function useBlossomServers() {
  * Default recommended Blossom servers
  */
 export const DEFAULT_BLOSSOM_SERVERS = [
+  'https://nostr.download',
   'https://blossom.band'
 ];
 
